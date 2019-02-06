e908021458ac989f0b0bb11304f359103e569eb0
ThomasPootQA <thomas.poot@qa.com>
2019-02-06 10:21:00 +0000

    Added other environment variables and removed un-needed ones

--- env.sh
+++ env.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-export RM_ADAPTER_HOSTNAME    =""
-export JOB_SERVICE_HOSTNAME   =""
-export RM_ADAPTER_INFO_URL    =""
-export JOB_SERVICE_INFO_URL   =""
-export RM_ADAPTER_QUEUES_URL  =""
-export JOB_SERVICE_QUEUES_URL =""
+# ops
+#export ACTION_SERVICE_XSD_URL=""
+#export RM_ADAPTER_XSD_URL=""
+#export LOCAL_TM_SWAGGER_SPEC=""
+export TM_HOSTNAME="tm"
+export RABBITMQ_HOSTNAME="rabbitmq"
+
+# smoke tests
+export RM_ADAPTER_HOSTNAME="fwmtgatewayrmadapter"
+export JOB_SERVICE_HOSTNAME="fwmtgatewayjobsvc"
