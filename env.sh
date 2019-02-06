6cc909c2468e8239dc42cb505dce17ddb7f9a6cc
ThomasPootQA <thomas.poot@qa.com>
2019-02-06 09:42:41 +0000

    Added an environment variable file, currently only with smoke test envs

---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export RM_ADAPTER_HOSTNAME    =""
+export JOB_SERVICE_HOSTNAME   =""
+export RM_ADAPTER_INFO_URL    =""
+export JOB_SERVICE_INFO_URL   =""
+export RM_ADAPTER_QUEUES_URL  =""
+export JOB_SERVICE_QUEUES_URL =""
