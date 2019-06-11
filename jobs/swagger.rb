23109dee76244e1932f8e5202a32d8f60cffcda2
garethTurner <gareth.turner@ons.gov.uk>
2019-06-11 16:20:11 +0100

    get tm swagger from gcs bucket

--- jobs/swagger.rb
+++ jobs/swagger.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 require 'swagger/diff'
+require 'json'
+require 'google/cloud/storage'
 
-LOCAL_TM_SWAGGER_SPEC = ENV['LOCAL_TM_SWAGGER_SPEC'] || 'https://raw.githubusercontent.com/ONSdigital/census-fwmt-job-service/master/src/main/resources/tm-swagger.json'
+if !ENV['GCP_DASHBOARD_KEYCONTENTS'].nil?
+  File.open('gcs_dashboard_keyfile.json', 'w') do |f|
+    f.write(ENV['GCP_DASHBOARD_KEYCONTENTS'])
+  end
+  GCP_DASHBOARD_KEYFILE = 'gcs_dashboard_keyfile.json'
+else
+  GCP_DASHBOARD_KEYFILE = '../keyfile/gcs_dashboard_keyfile.json'
+end
 
 TM_HOSTNAME = ENV['TM_HOSTNAME'] || 'tm'
 TM_PORT = ENV['TM_PORT'] || '80'
@@ -19,8 +28,24 @@ def send_swagger_fatal(id, exception)
                       moreinfo: exception.message, fatal: true)
 end
 
+def fetch_swagger
+  storage = Google::Cloud::Storage.new(
+    project_id: 'census-fwmt-ci-233109',
+    credentials: GCP_DASHBOARD_KEYFILE
+  )
+
+  bucket = storage.bucket 'census-fwmt-acceptance-tests'
+  file = bucket.file 'tm-swagger.json'
+
+  # Download the file to the local file system
+  file.download 'public/swagger/tm-swagger.json'
+
+  JSON.parse(File.read('public/swagger/tm-swagger.json'))
+end
+
 SCHEDULER.every '30s', first_in: 0 do |_|
-  diff = Swagger::Diff::Diff.new(LOCAL_TM_SWAGGER_SPEC, TM_SWAGGER_SPEC)
+  local_json = fetch_swagger
+  diff = Swagger::Diff::Diff.new(local_json, TM_SWAGGER_SPEC)
   msg = diff.compatible? ? 'Compatible' : 'Incompatible'
   send_swagger_status('compare_swagger_files', msg, danger: !diff.compatible?)
 rescue StandardError => e
